3eff759c1a1a1756e42865ded4483809edf6450a
jonss0777 <jsuconota1@gmail.com>
2025-02-08 18:55:22 -0500

    adding audio logic

--- ui/index.py
+++ ui/index.py
@@ -1,16 +1,29 @@
 import streamlit as st
 
+
+
 # Record audio using st.audio_input
 audio_data = st.audio_input("Record your audio")
 
-if audio_data is not None:
-    # The returned audio_data is an UploadedFile object
-    # Read the audio data into bytes
-    audio_bytes = audio_data.read()
 
-    # Save the audio data to a file
-    with open("recorded_audio.wav", "wb") as f:
-        f.write(audio_bytes)
+# call back to save voice audio
+def save_file():
+    if audio_data is not None:
+        # The returned audio_data is an UploadedFile object
+        # Read the audio data into bytes
+        audio_bytes = audio_data.read()
 
-    # Optionally, play the recorded audio
-    st.audio(audio_bytes, format='audio/wav')
+        # Save the audio data to a file
+        with open("recorded_audio.wav", "wb") as f:
+            f.write(audio_bytes)
+
+    
+# plays an existing voice audio
+def play_audio(filename): 
+    st.audio(filename, format='audio/wav')
+    
+
+
+
+
+st.button("Save audio as", key=None, help=None, on_click=save_file, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=False)
---
+++ ui/pages/display.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Navbar import streamlit as st
+
+st.page_link("./index.py", label="Home", icon="🏠")
+st.page_link("pages/display.py", label="Page 1", icon="1️⃣")
+
+#st.text_area("from_language", value=None, height=None, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
+
+st.title("healthierlife", anchor=None, help=None)
+#st.header("Header tag")
+
+def translation():
+    pass
+    
+    
+word = "Hello World"
+    
+st.feedback("stars")
+
+
+st.write("{word}")
+
+
